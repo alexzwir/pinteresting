d0d8a2ad610cecd8f010dc08c84bb2a75547d09b
alexzwir <alexzwir@gmail.com>
2015-05-10 22:09:41 -0300

    Cofiguring the pins to be acessible only to the users that are logged in

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -10,8 +11,6 @@ class PinsController < ApplicationController
   end
 
   def new
-    #@pin = Pin.new
-    # We are going to replace to a devise helper method called current_user
     @pin = current_user.pins.build
   end
 
@@ -44,6 +43,11 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "You're not authorize to change this pin." if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
