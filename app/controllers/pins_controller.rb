e11966b31e11e2e8cab0109fc49c54b766b2bc97
alexzwir <alexzwir@gmail.com>
2015-05-20 07:57:19 -0300

    Listing in the index file by the most recent pins to the lastest pin posted by the user - DESC

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
@@ -55,3 +55,4 @@ class PinsController < ApplicationController
       params.require(:pin).permit(:description, :image)
     end
 end
+
